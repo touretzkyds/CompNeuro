ad924946828005420779c8a48f1966354e67c8e5
Dave Touretzky <dst@cs.cmu.edu>
2025-08-25 00:27:01 -0400

    Updates

--- cmac1/setup_random_buckets.m
+++
@@ -1,8 +0,0 @@
-% setup_random_buckets
-
-bins_assigned = {};
-for z = 0:359
-  c = find((z>=bmin|z<bmax-360) & z<=bmax);
-  bins_assigned{z+1} = buckets(c);
-  bincounts(z+1) = length(c);
-end
--- cmac1/setup_systematic_buckets.m
+++
@@ -1,8 +0,0 @@
-% systematic_buckets
-
-bins_assigned = {};
-for z = 0:359
-  c = find((z>=bmin|z<bmax-360) & z<=bmax);
-  bins_assigned{z+1} = c;
-end
-
